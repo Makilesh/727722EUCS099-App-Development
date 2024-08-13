d814f728fefcd32f8268b5a988805ac232663ef9
Makilesh M <125253325+Makilesh@users.noreply.github.com>
2024-08-13 09:29:20 +0530

    Add files via upload

---
+++ Day 6/ModelandRepo/src/main/java/com/example/giftportal/model/Address.java
@@ -0,0 +1,38 @@
+package com.example.giftportal.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String streetName;
+    private String city;
+    private String district;
+    private String state;
+    private String pinCode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+}
